558c8fcbe7f998444584b8b81828465fc743fdc9
user <user@debian>
2023-11-10 12:04:56 +0100

    modif py bdd

--- Rodelika/Rodelika.py
+++ Rodelika/Rodelika.py
@@ -11,58 +11,92 @@ def print_hello_message():
     print ("-----------------------------------")
 
 def print_menu():
+    print("")
     print (" 1 - Afficher la liste des étudiants ")
     print (" 2 - Afficher le sold des étudiants ")
     print (" 3 - Saisir un nouvel étudiant ")
     print (" 4 - Attribuer un bonus ")
     print (" 5 - Quitter")
 
+from tabulate import tabulate
+
 def get_list_student():
-    sql="select etu_num,etu_nom,etu_prenom from Etudiant"
+    sql = "SELECT etu_num, etu_nom, etu_prenom FROM Etudiant"
     cursor = cnx.cursor()
     cursor.execute(sql)
-    row = cursor.fetchone()
-    while row is not None:
-        print(row)
-        row = cursor.fetchone()
+    rows = cursor.fetchall()
+
+    if rows:
+        headers = ["Numéro Etudiant", "Nom", "Prénom"]
+        table = tabulate(rows, headers=headers, tablefmt="pretty")
+        print(table)
+    else:
+        print("Aucun étudiant trouvé dans la base de données.")
 
 def get_list_student_with_sold():
-    sql="""select etudiant.*, sum(compte.opr_montant) as sold from Etudiant,
-    compte where etudiant.etu_num = compte.etu_num group by compte.etu_num"""
+    sql = "SELECT etu_num, etu_nom, etu_prenom, etu_solde, etu_bonus FROM Etudiant"
     cursor = cnx.cursor()
     cursor.execute(sql)
-    row = cursor.fetchone()
-    while row is not None:
-        print(row)
-        row = cursor.fetchone()
+    rows = cursor.fetchall()
+
+    if rows:
+        headers = ["Numéro Etudiant", "Nom", "Prénom", "Solde", "Bonus"]
+        table = tabulate(rows, headers=headers, tablefmt="pretty")
+        print(table)
+    else:
+        print("Aucun étudiant trouvé dans la base de données.")
+
 
 def new_student():
     num_etudiant = input("Numéro Etudiant : ")
 
-    #Vérifie si le num-etudiant existe ou non
-    check_query = "SELECT COUNT(*) FROM etudiant WHERE etu_num = %s;"
+    # Vérifiez si le numéro d'étudiant existe déjà
+    check_query = "SELECT COUNT(*) FROM Etudiant WHERE etu_num = %s;"
     check_val = (num_etudiant,)
+
+    cursor = cnx.cursor()
     cursor.execute(check_query, check_val)
     result = cursor.fetchone()
-    
+
     if result[0] > 0:
-        print("Le numéro d'étudiant existe déja.")
-    else
-    
-    nom_etudiant = input("Nom Etudiant : ")
-    prenom_etudiant = input("Prénom Etudiant : ")
-    
-    #Ajoute un nouvel étudiant à la BDD
-    sql = """INSERT INTO etudiant (etu_num, etu_nom, etu_prenom) VALUES (NULL, %s,%s);""" #Requete sql
-    val = (num_etudiant, nom_etudiant, prenom_etudiant) #Insere les valeurs dans la base de donnees
-    cursor = cnx.cursor() #Creation du curseur 
-    cursor.execute(sql, val) #Execute la requete sql sur la base de donnees
-    cnx.commit() #Valide les changements effectués dans la base de donnees
+        print("Le numéro d'étudiant existe déjà.")
+    else:
+        nom_etudiant = input("Nom Etudiant : ")
+        prenom_etudiant = input("Prénom Etudiant : ")
+        solde_etudiant = input("Solde etudiant : ")
+        bonus_etudiant = input("Bonus_etudiant : ")
+
+        # Ajoute un nouvel étudiant à la BDD
+        sql = """INSERT INTO Etudiant (etu_num, etu_nom, etu_prenom, etu_solde, etu_bonus) VALUES (%s, %s, %s, %s, %s);"""
+        val = (num_etudiant, nom_etudiant, prenom_etudiant, solde_etudiant, bonus_etudiant)
+
+        cursor.execute(sql, val)
+        cnx.commit()
+        print("Nouvel étudiant ajouté avec succès.")
+
+    # N'oubliez pas de fermer le curseur après utilisation.
+    cursor.close()
+
+# Assurez-vous d'avoir la connexion à la base de données (cnx) avant d'appeler la fonction.
+# Remplacez les valeurs entre crochets par les informations appropriées.
+# cnx = mysql.connector.connect(user='[votre_utilisateur]', password='[votre_mot_de_passe]', host='[votre_host]', database='[votre_base_de_donnees]')
+
+# Appelez la fonction
+# new_student()
+
+# N'oubliez pas de fermer la connexion après utilisation.
+# cnx.close()
+
+
 
 def add_bonus():
     num = input("Numéro Etudiant : ")
     #com = input("Commentaire : ")
-    
+    bonus = 1.00
+    sql = "UPDATE Etudiant SET etu_bonus = etu_bonus + %s WHERE etu_num = %s;"
+    cursor = cnx.cursor()
+    cursor.execute(sql,(bonus,num))
+    cnx.commit()
     # compléter le code
     print ("Bonus + 1.00 euros")
 
