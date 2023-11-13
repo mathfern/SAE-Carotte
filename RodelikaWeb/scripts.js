77b37ff33d07c72eafcf12dad795836287db5b59
user <user@debian>
2023-11-13 14:39:02 +0100

    modif script

--- RodelikaWeb/scripts.js
+++ RodelikaWeb/scripts.js
@@ -6,8 +6,6 @@ function getStudents() {
         dataType: "json",
         success: function (response) {
             displayResults(response);
-            // Masque les résultats après l'affichage
-            goBack();
         },
         error: function (error) {
             console.log("Error:", error);
@@ -22,47 +20,38 @@ function getSold() {
         data: { choice: 2 },
         dataType: "json",
         success: function (response) {
-            displayResults(response);
-            // Masque les résultats après l'affichage
-            goBack();
+            displayResultsSolde(response);
         },
         error: function (error) {
             console.log("Error:", error);
         }
     });
 }
-function getStudents() {
-    $.ajax({
-        type: "POST",
-        url: "call_python_function.php",
-        data: { choice: 1 },
-        dataType: "json",
-        success: function (response) {
-            displayResults(response);
-        },
-        error: function (error) {
-            console.log("Error:", error);
-        }
-    });
-}
-
-function getSold() {
-    $.ajax({
-        type: "POST",
-        url: "call_python_function.php",
-        data: { choice: 2 },
-        dataType: "json",
-        success: function (response) {
-            displayResults(response);
-        },
-        error: function (error) {
-            console.log("Error:", error);
-        }
-    });
-}
-
 
 function displayResults(results) {
+    $("#result").empty();
+
+    if (results.length > 0) {
+        var table = "<table border='1'>";
+        table += "<tr><th>Numéro Etudiant</th><th>Nom</th><th>Prénom</th></tr>";
+
+        for (var i = 0; i < results.length; i++) {
+            table += "<tr>";
+            table += "<td>" + results[i].etu_num + "</td>";
+            table += "<td>" + results[i].etu_nom + "</td>";
+            table += "<td>" + results[i].etu_prenom + "</td>";
+            table += "</tr>";
+        }
+
+        table += "</table>";
+        $("#result").html(table);
+    } else {
+        $("#result").html("Aucun résultat trouvé.");
+    }
+}
+
+
+function displayResultsSolde(results) {
     $("#result").empty();
 
     if (results.length > 0) {
