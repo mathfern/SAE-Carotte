a019a1ef60c8bde4eaf9663de6e20a89a5f7370d
user <user@debian>
2023-11-14 16:56:45 +0100

    Berlicum

--- Berlicum/Berlicum.py
+++ Berlicum/Berlicum.py
@@ -36,7 +36,6 @@ def __print_apdu(apdu):
             print("0x%02X" % x, end=' ')
         print("\n")  
 
-
 def print_hello_message():
 	print ("-------------------------------------------------------------------")
 	print ("Bienvenue sur Berlicum, le logiciel de la borne de recharge")
@@ -44,11 +43,101 @@ def print_hello_message():
 	print ("\n")
 
 def print_menu():
-	print ("1 - Vérifier l'existance du compte sur la Purple Dragon")
-	print ("2 - Afficher mes informations")
-	print ("3 - Consulter mes bonus")
-	print ("4 - Transférer mes bonus sur la carte")
-	print ("5 - Consulter mon crédit disponible sur la carte")
-	print ("6 - Recharger par carte bancaire")
-	print ("7 - Afficher l'historique des transactions")
-	print ("8 - Quitter")
+	print ("1 - Afficher mes informations")
+	print ("2 - Consulter mes bonus")
+	print ("3 - Transférer mes bonus sur la carte")
+	print ("4 - Consulter mon crédit disponible sur la carte")
+	print ("5 - Recharger par carte bancaire")
+	print ("6 - Afficher l'historique des transactions")
+	print ("7 - Quitter")
+
+def affiche_info():
+	apdu = [0x80, 0x04, 0x00, 0x00, 0x01]
+	data, sw1, sw2 = conn_reader.transmit(apdu)
+	
+	print ("sw1 : 0x%02X | sw2 : 0x%02X" % (sw1,sw2))
+
+	apdu[4] = sw2
+	data, sw1, sw2 = conn_reader.transmit(apdu)
+	infos = ""
+	for e in data:
+		infos += chr(e)
+	num_etudiant = int(infos.split()[-1])
+	# print (type(num_etudiant))
+	# Vérifiez si le numéro d'étudiant existe déjà
+	check_query = "SELECT COUNT(*) FROM Etudiant WHERE etu_num = %s;"
+	check_val = (num_etudiant,)
+
+	cursor = cnx.cursor()
+	cursor.execute(check_query, check_val)
+	result = cursor.fetchone()
+
+	if result[0] < 0:
+		print("Ce numéro d'étudiant n'existe pas allez voir l'agent administratif")
+	else: 
+		sql = "SELECT etu_num, etu_nom, etu_prenom FROM Etudiant WHERE etu_num = %s;"
+		cursor.execute(sql, (num_etudiant,))
+		student_info = cursor.fetchone()
+		print("Informations sur l'étudiant :", student_info)
+	return
+
+def affiche_bonus():
+	apdu = [0x80, 0x04, 0x00, 0x00, 0x01]
+	data, sw1, sw2 = conn_reader.transmit(apdu)
+	
+	print ("sw1 : 0x%02X | sw2 : 0x%02X" % (sw1,sw2))
+
+	apdu[4] = sw2
+	data, sw1, sw2 = conn_reader.transmit(apdu)
+	infos = ""
+	for e in data:
+		infos += chr(e)
+	num_etudiant = int(infos.split()[-1])
+	# print (type(num_etudiant))
+	# Vérifiez si le numéro d'étudiant existe déjà
+	check_query = "SELECT COUNT(*) FROM Etudiant WHERE etu_num = %s;"
+	check_val = (num_etudiant,)
+
+	cursor = cnx.cursor()
+	cursor.execute(check_query, check_val)
+	result = cursor.fetchone()
+
+	if result[0] < 0:
+		print("Ce numéro d'étudiant n'existe pas allez voir l'agent administratif")
+	else: 
+		sql = "SELECT etu_bonus FROM Etudiant WHERE etu_num = %s;"
+		cursor.execute(sql, (num_etudiant,))
+		student_info = cursor.fetchone()
+		print("Vous avez :", student_info , " bonus")
+	return
+
+
+
+
+def main():
+	print_hello_message()
+	init_smart_card()
+	while True:
+		print_menu()
+		cmd = int(input("Choix :"))
+		if (cmd == 1):
+			affiche_info()		
+		elif (cmd == 2):
+			affiche_bonus()
+		# elif (cmd == 3):
+		# 	assign_card()
+		# elif (cmd == 4):
+		# 	init_sold()
+		# elif (cmd == 5):
+		# 	consult_sold()
+		# elif (cmd == 6):
+		# 	delete_data()
+		elif (cmd == 7):
+			return
+		else :
+			print ("erreur, saisissez une commande valide")
+			return
+	print_menu()
+
+if __name__ == '__main__':
+	main()
